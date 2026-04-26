40ae60a8ad6bbf8e40aee6404268fa11dcb69e59
RXOXOR <rajendravanjarapu21@gmail.com>
2026-04-26 16:18:55 +0530

    Update SocialApplication.java

--- social-media-app/backend/src/main/java/com/example/social/SocialApplication.java
+++ social-media-app/backend/src/main/java/com/example/social/SocialApplication.java
@@ -2,9 +2,17 @@ package com.example.social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SocialApplication {
+public class SocialApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+        return app.sources(SocialApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SocialApplication.class, args);
     }
