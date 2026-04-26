4818f8c0e107aecad16b746e7ea1d2debab0dc95
RXOXOR <rajendravanjarapu21@gmail.com>
2026-04-26 15:41:43 +0530

    Update UserRepository.java

--- social-media-app/backend/src/main/java/com/example/social/repository/UserRepository.java
+++ social-media-app/backend/src/main/java/com/example/social/repository/UserRepository.java
@@ -1,1 +1,7 @@
+package com.example.social.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.social.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
