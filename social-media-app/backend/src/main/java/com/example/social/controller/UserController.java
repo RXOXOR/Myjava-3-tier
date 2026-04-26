141a110ab7e33e73f2cbe69dfd8e97daa45bbefd
RXOXOR <rajendravanjarapu21@gmail.com>
2026-04-26 15:42:52 +0530

    Update UserController.java

--- social-media-app/backend/src/main/java/com/example/social/controller/UserController.java
+++ social-media-app/backend/src/main/java/com/example/social/controller/UserController.java
@@ -1,1 +1,26 @@
+package com.example.social.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import com.example.social.entity.User;
+import com.example.social.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return service.save(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return service.getAll();
+    }
+}
