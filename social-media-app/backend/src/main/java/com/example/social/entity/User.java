671251df39c7a4685466e2562b306721b928f715
RXOXOR <rajendravanjarapu21@gmail.com>
2026-04-26 15:40:53 +0530

    Update User.java

--- social-media-app/backend/src/main/java/com/example/social/entity/User.java
+++ social-media-app/backend/src/main/java/com/example/social/entity/User.java
@@ -1,1 +1,17 @@
+package com.example.social.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+}
